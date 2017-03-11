2d20e74cdc8604b4f71004c7ef0ba9216d8f8c36
Eric Zhang <eric.quin.zhang@gmail.com>
2017-03-11 17:22:20 -0500

    Solved merge

--- Assets/Scripts/Behaviour/LightBehaviour.cs
+++ Assets/Scripts/Behaviour/LightBehaviour.cs
@@ -9,17 +9,31 @@ public class LightBehaviour : MonoBehaviour {
 	public GameTime sunset;
 	public GameTime twilight; 
 
+	private Light[] day_lights; 
+	private Light[] night_lights; 
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		sunrise = new GameTime (-1, -1, 5, 0, 0);
 		highnoon = new GameTime (-1, -1, 12, 0, 0);
 		sunset = new GameTime (-1, -1, 20, 0, 0);
 		twilight = new GameTime (-1, -1, 23, 59, 59);
+
+		foreach (Transform child in GameObject.Find("DayLights").transform) {
+		}
+
+		foreach (Transform child in GameObject.Find("NightLights").transform) {
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GameTime curtime = Game.instance ().getCurrentGameTime ();
-
+//		GameTime curtime = Game.instance ().getCurrentGameTime ();
+//		if (curtime < sunrise) {
+//			
+//		} else if (curtime < highnoon) {
+//		} else if (curtime < sunset) {
+//		} else if (curtime < twilight) {
+//		}
 	}
 }
