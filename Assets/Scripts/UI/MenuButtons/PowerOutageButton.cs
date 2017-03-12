ec16f3fa1545aecd4a0db0088183e7b3d7cee32e
Eric Zhang <eric.quin.zhang@gmail.com>
2017-03-12 06:41:48 -0400

    Added effect to electric outage

--- Assets/Scripts/UI/MenuButtons/PowerOutageButton.cs
+++ Assets/Scripts/UI/MenuButtons/PowerOutageButton.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PowerOutageButton : MenuButton {
 
+	public GameObject animation;
+
     protected override void perform()
     {
         PowerOuttageAction act = new PowerOuttageAction();
         FireActionInteraction inter = new FireActionInteraction(act);
+		var electric = Instantiate(animation);
+		electric.transform.position = new Vector3();
         inter.execute();
     }
 }
